724e0a6f60109172efda29dd961626eb5c444a58
hebaoning <baoning.he@ecreditpal.com>
2018-01-23 16:23:49 +0800

    Add testcase example

---
+++ src/test/java/demo/spring/controller/HelloControllerTest.java
@@ -0,0 +1,48 @@
+package demo.spring.controller;
+
+import demo.spring.config.RootConfig;
+import demo.spring.config.WebConfig;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration("src/main/webapp")
+@ContextHierarchy({
+        @ContextConfiguration(classes = RootConfig.class),
+        @ContextConfiguration(classes = WebConfig.class)
+})
+public class HelloControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+        mockMvc.perform(get("/user").accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
